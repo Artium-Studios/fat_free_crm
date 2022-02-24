0f2311892496c9239e7d858cda044d7344318fb0
Ryan Durling <ryan.p.durling@gmail.com>
2022-02-24 08:58:01 -0600

    update ruby version in Dockerfile; add act_as_commentable to vendor gems;

---
+++ vendor/gems/acts_as_commentable-rails-6/test/schema.rb
@@ -0,0 +1,14 @@
+ActiveRecord::Schema.define(version: 0) do
+  create_table :posts do |t|
+    t.text :text
+    t.timestamps
+  end
+
+  create_table :users do |u|
+    u.string :name
+  end
+
+  create_table :walls do |w|
+    w.string :name
+  end
+end
